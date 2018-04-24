0056c60d2d024c4474b5476644589b5d9102f939
Michalfsn <mkungonda@gmail.com>
2018-04-24 21:42:57 +0200

    first view and activity

---
+++ RaysHotDogs/HotDogDetailActivity.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.Utility;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "Hot dog details", MainLauncher = true)]
+    public class HotDogDetailActivity : Activity
+    {
+        private ImageView hotDogImageView;
+        private TextView hotDogNameTextView;
+        private TextView shortDescriptionTextView;
+        private TextView descriptionTextView;
+        private TextView priceTextView;
+        private EditText amountEditView;
+        private Button cancelButton;
+        private Button orderButton;
+
+        private HotDog selectedHotDog;
+        private HotDogDataService dataService;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.HotDogDetailView);
+
+            HotDogDataService dataService = new HotDogDataService();
+            selectedHotDog = dataService.GetHodDogById(1);
+
+            FindViews();
+            BindData();
+            HandlEvents();
+        }
+
+        private void FindViews()
+        {
+            hotDogImageView = FindViewById<ImageView>(Resource.Id.hotDogImageView);
+            hotDogNameTextView = FindViewById<TextView>(Resource.Id.hotDogNameTextView);
+            shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            amountEditView = FindViewById<EditText>(Resource.Id.amountEditText);
+            cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+            orderButton = FindViewById<Button>(Resource.Id.orderButton);
+        }
+
+        private void BindData()
+        {
+            hotDogNameTextView.Text = selectedHotDog.Name;
+            shortDescriptionTextView.Text = selectedHotDog.ShortDescription;
+            descriptionTextView.Text = selectedHotDog.Description;
+            priceTextView.Text = "Price: " + selectedHotDog.Price;
+
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl("https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+            hotDogImageView.SetImageBitmap(imageBitmap);
+        }
+
+        private void HandlEvents()
+        {
+            orderButton.Click += OrderButton_Click;
+            cancelButton.Click += CancelButton_Click;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            var amount = Int32.Parse(amountEditView.Text);
+
+            var dialog = new AlertDialog.Builder(this);
+            dialog.SetTitle("Confirmation");
+            dialog.SetMessage("Your hot dog has been added to your cart!");
+            dialog.Show();
+        }
+    }
+}
--- RaysHotDogs/Resources/Resource.Designer.cs
+++ RaysHotDogs/Resources/Resource.Designer.cs
@@ -41,11 +41,57 @@ namespace RaysHotDogs
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040008
+			public const int amountEditText = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int cancelButton = 2130968583;
+			
+			// aapt resource value: 0x7f040004
+			public const int descriptionTextView = 2130968580;
+			
+			// aapt resource value: 0x7f040001
+			public const int hotDogImageView = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int hotDogNameTextView = 2130968578;
+			
+			// aapt resource value: 0x7f040000
+			public const int mainScrollView = 2130968576;
+			
+			// aapt resource value: 0x7f040009
+			public const int orderButton = 2130968585;
+			
+			// aapt resource value: 0x7f040005
+			public const int priceTextView = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int relativeLayout1 = 2130968582;
+			
+			// aapt resource value: 0x7f040003
+			public const int shortDescriptionTextView = 2130968579;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int HotDogDetailView = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Main = 2130837505;
 			
 			static Layout()
 			{
